cbaaf272940850b9e122d84d2f3eaab86787fb28
kenichi220 <kenichibrumati.k@gmail.com>
2026-04-13 08:54:45 -0300

    nes

--- install.sh
+++ install.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
+
+sudo dnf update -y
+
+# gcc, g++, make, git
+echo "----------------------"
+echo "---gcc,g++,make,git---"
+echo "----------------------"
+sudo dnf install -y gcc gcc-c++ make git
+
+
+
 # INSTALL KITTY
 # INSTALL R
+echo "------------------"
+echo "----Install R-----"
+echo "------------------"
+
+sudo dnf install -y R libcurl-devel openssl-devel libxml2-devel harfbuzz-devel fribidi-devel
+
+PACK_R="'ggplot2', 'dplyr', 'tidyr', 'readr', 'jsonlite'"
+
+sudo Rscript -e "install.packages(c($PACK_R), repos='https://cloud.r-project.org/', dependencies=TRUE)"
+
+R --version | head -n 1
+
 # INSTALL ZSH
 # INSTALL ZSH THEME
+# CALL ZSH INSTALL
+
+
 # INSTALL NVIM
 # CONFIG NVIM
+
+
+
+
 # INSTALL NIX
 #
 
